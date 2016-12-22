176b87a19330197e778c4dfed5a011fc3d7ae753
Lochie Ferrier <lochieferrier@gmail.com>
2016-12-21 21:44:39 -0500

    Heroku stuff

--- webInterface/app.py
+++ webInterface/app.py
@@ -3,7 +3,14 @@ from flask.ext.bower import Bower
 app = Flask(__name__)
 @app.route("/")
 def main():
-	return "Hi!"
+	return "wrong thing"
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     WSGIServer(('0.0.0.0', int(os.environ.get("PORT", 5000))), app).serve_forever()
+
+
 if __name__ == "__main__":
-	app.run()
+	port = int(os.environ.get("PORT"),5000)
+	app.run('0.0.0.0',port=port,debug=True)
 Bower(app)
